a1796b3cff9780bd74b72cdde56964546c086cc6
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2023-08-06 18:37:23 -0500

    Added comments for better readability. Completed Assignment

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 window.addEventListener("load", function() {
     let form = document.querySelector("form");
+    //// Event listener for when the form is submitted
     form.addEventListener("submit", function(event){
         let document = window.document;
         let userPilotName = document.querySelector("input[name=pilotName]");
@@ -9,13 +10,14 @@ window.addEventListener("load", function() {
         let userFuelLevel = document.querySelector("input[name=fuelLevel]");
         let userCargoMass = document.querySelector("input[name=cargoMass]");
 
-
+        // Check if any input fields are empty
         if (userPilotName.value === "" || userCoPilotName.value === "" || userFuelLevel.value === "" || userCargoMass.value === ""){
             alert("All fields are required!");
             event.preventDefault();
         } else {
+            // Call the formSubmission function if all input fields are filled
             formSubmission(document, userPilotName, userCoPilotName, userFuelLevel, userCargoMass)
-            event.preventDefault();
+            event.preventDefault(); 
         };
     });
 
@@ -29,12 +31,14 @@ window.addEventListener("load", function() {
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+
+       //Pick a planet randomly from list
        let planetPicked = pickPlanet(listedPlanets);
        console.log(planetPicked);
        
         console.log(planetPicked.name);
-
-       addDestinationInfo(document, planetPicked.name, planetPicked.diameter, planetPicked.star, planetPicked.distance, planetPicked.moons, planetPicked.image);
+        //Adds information about the picked planet
+        addDestinationInfo(document, planetPicked.name, planetPicked.diameter, planetPicked.star, planetPicked.distance, planetPicked.moons, planetPicked.image);
     })
    
 });
